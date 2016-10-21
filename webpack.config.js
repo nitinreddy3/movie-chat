eda5ac02ff8e63d6ce70effc5115508aed5fb0a9
Nitin Reddy <reddynitin3@gmail.com>
2016-10-21 09:03:19 +0530

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,31 @@
-var webpack = require('webpack')
-var path = require('path')
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-	entry: {
-		app: './src/app.js'
-	},
-	output: {
-		filename: 'build/bundle.js',
-		sourceMapFilename: 'build/bundle.map'
-	},
-	devtool: '#source-map',
-	module: {
-		loaders: [
-			{
-				test: /\.js?$/,
-				exclude: /(node_modules)/,
-				loader: 'babel',
-				query: {
-					presets: ['react', 'es2015']
-				}
-			}
-		]
-	}
-}
+  entry: [
+    './src/index'
+  ],
+  module: {
+    loaders: [
+      { test: /\.js?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.s?css$/, loader: 'style!css!sass' },
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]
+};
